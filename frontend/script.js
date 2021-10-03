20ca0dd9fb05ca67b14edc0de90ab4464191e93d
Csikusz PC <davidcsirak@yahoo.com>
2021-10-03 18:40:50 +0200

    Online users interface added

--- frontend/script.js
+++ frontend/script.js
@@ -10,10 +10,12 @@ const socket = io('http://localhost:3000', {
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
+const userContainer = document.getElementById('users-container')
 
 //const name = prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', name)
+socket.emit('list_users')
 
 socket.on('message', data => {
     appendMessage(`${data.from}: ${data.data}`)
@@ -21,11 +23,17 @@ socket.on('message', data => {
 
 socket.on('user-connected', name => {
     appendMessage(`${name} connected`)
+    appendUser(name)
   })
 
 socket.on('user-disconnected', name => {
     appendMessage(`${name} disconnected`)
+    deleteUser(name)
   })
+
+socket.on('list_users', users => {
+  users.map(user => appendUser(user))
+})
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
@@ -40,4 +48,16 @@ function appendMessage(message) {
     const messageElement = document.createElement('div')
     messageElement.innerText = message
     messageContainer.append(messageElement)
+}
+
+function appendUser(user) {
+  const userElement = document.createElement('div')
+  userElement.setAttribute("id", user)
+  userElement.innerText = user
+  userContainer.append(userElement)
+}
+
+function deleteUser(user) {
+  const userElement = document.getElementById(user)
+  userContainer.removeChild(userElement)
 }
