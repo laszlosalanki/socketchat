0b04d56163295f44e623dcf9daf2d395535a3211
K-Daniel98 <kantordani24@gmail.com>
2021-11-14 13:32:28 +0100

    Updated UserModel

--- backend/src/user/models/user.schema.ts
+++ backend/src/user/models/user.schema.ts
@@ -4,11 +4,23 @@ import { Document } from 'mongoose';
 export type UserDocument = UserModel & Document;
 
 @Schema()
+class PersonalizationModel {
+  @Prop({ required: true })
+  public background: string;
+}
+
+@Schema()
 export class UserModel {
   @Prop({ required: true })
   public username: string;
-  @Prop({ required: true })
+  @Prop({ required: false })
   public password: string;
+  @Prop({ required: false, unique: true, default: null })
+  public email: string;
+  @Prop({ required: false, default: 0.0 })
+  public spent_time: number;
+  @Prop({ required: false, default: null })
+  public personalization: PersonalizationModel;
 }
 
 export const UserSchema = SchemaFactory.createForClass(UserModel);
